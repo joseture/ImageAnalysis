f3299531139e2b9953190b6749ec66229ea42185
joseture <30830916+joseture@users.noreply.github.com>
2022-10-04 17:09:15 +0200

    First

---
+++ Exercise1/Ex1.py
@@ -0,0 +1,13 @@
+import numpy as np
+from skimage import color, io, measure, img_as_ubyte
+from skimage.measure import profile_line
+from skimage.transform import rescale, resize
+import matplotlib.pyplot as plt
+import pydicom as dicom
+
+
+# Directory containing data and images
+in_dir = "data/"
+
+print("hello")
+
